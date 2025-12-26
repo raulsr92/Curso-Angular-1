3b1fc6b2c6702105511d1b3e751691bbf86f4a2a
Raul Sanchez R. <raul.sr92@gmail.com>
2025-12-26 18:22:35 -0500

    Clase 2-Creacion de proyecto y renderizado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todoAppRaul');
+
+  city = 'Lima';
+  country = "Peru"
+  tasks = [
+    "Instalar Angular CLI",
+    "Crear proyecto",
+    "Crear componentes"
+  ]
+
+  fruits = [
+    "Manzana",
+    "Pera",
+    "Naranja"
+  ]
 }
