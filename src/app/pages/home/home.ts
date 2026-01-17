7e2e05fa4182ca1f92c50e134c5b2255c3bc28e2
Raul Sanchez R. <raul.sr92@gmail.com>
2026-01-16 19:39:06 -0500

    Gestión dinamica de tareas en JS: Crear y eliminar

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -14,4 +14,32 @@ export class Home {
     "Crear componentes",
     "Añadir signals"
   ]);
+
+
+  changeHandler = (event: Event)=>{
+
+    //Capturar tarea
+
+    const inputElement = event.target as HTMLInputElement
+    const newTask =inputElement.value
+
+    console.log(`Nueva tarea ingresada: ${newTask}`)
+
+    // Agregar tarea al array de tareas (recuerda que es signal)
+
+    this.tasks2.update((array)=> [...array,newTask] )
+
+    // Limpiar input
+
+    inputElement.value= ""
+
+  }
+
+  deleteTask = (index: number) =>{
+
+    // Eliminar la tarea
+
+     this.tasks2.update((array) => array.filter((item, posicion)=> posicion !== index))
+
+  }
 }
